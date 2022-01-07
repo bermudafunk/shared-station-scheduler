f75f5ebb12ad8c5ec83fd3a7527bba48e00ee030
Christian Kohlstedde <christian@kohlsted.de>
2022-01-07 18:16:07 +0100

    Test setting PS instead of selecting DataSet

--- uecp_test.py
+++ uecp_test.py
@@ -2,11 +2,10 @@ import asyncio
 import itertools
 from asyncio import StreamWriter
 
-from uecp.commands import DataSetSelectCommand
+from uecp.commands import ProgrammeServiceNameSetCommand
 from uecp.frame import UECPFrame
 
 from scheduler import config
-from scheduler.calendar_provider import Programme
 from scheduler.rds import open_serial_writer
 
 sequence_number = itertools.cycle(range(1, 256))
@@ -15,12 +14,12 @@ sequence_number = itertools.cycle(range(1, 256))
 async def set_rds_encoder_state(uecp_writer: StreamWriter):
     for programme in itertools.cycle(
         [
-            Programme.radioaktiv,
+            "R-AKTIV",
+            "bermuda",
         ]
     ):
-        print(f"Set active dataset to {programme}")
-        rds_dataset = programme.rds_dataset
-        command = DataSetSelectCommand(select_data_set_number=rds_dataset)
+        print(f"Set ps to {programme}")
+        command = ProgrammeServiceNameSetCommand(ps=programme)
 
         frame = UECPFrame()
         frame.sequence_counter = next(sequence_number)
@@ -30,7 +29,7 @@ async def set_rds_encoder_state(uecp_writer: StreamWriter):
         uecp_writer.write(data)
         print(data.hex())
         await asyncio.wait_for(uecp_writer.drain(), 10)
-        await asyncio.sleep(5)
+        await asyncio.sleep(15)
 
 
 async def main():
