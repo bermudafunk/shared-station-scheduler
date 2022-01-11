b4e0f06c4a072d9c632a3c803ba14417bc85723a
Christian Kohlstedde <christian@kohlsted.de>
2022-01-12 00:13:46 +0100

    Add furthr tests

--- tests/test_calendar_provider.py
+++ tests/test_calendar_provider.py
@@ -2,6 +2,7 @@ import asyncio
 import functools
 from datetime import datetime
 
+import icalevents
 import pytest
 from dateutil.tz import gettz
 from pytest_httpserver import HTTPServer
@@ -79,3 +80,67 @@ class TestProgrammeEventProvider:
             await pep.refreshEventTask
         except asyncio.CancelledError:
             pass
+
+    @now(datetime(2022, 1, 11, 7, 0, 1, tzinfo=tz))
+    @pytest.mark.asyncio
+    async def test_pep3(self, shared_datadir, httpserver: HTTPServer):
+        content = (shared_datadir / "test_ics_3").read_text()
+        httpserver.expect_request("/ics").respond_with_data(content)
+
+        ccme = calendar_provider.check_continuous_monotonic_events
+
+        def null_op(*_, **__):
+            pass
+
+        calendar_provider.check_continuous_monotonic_events = null_op
+
+        pep = await calendar_provider.ProgrammeEventProvider.create(
+            ics_url=httpserver.url_for("/ics"), tz=tz
+        )
+
+        assert pep.active_event is not None
+
+        calendar_provider.check_continuous_monotonic_events = ccme
+
+    def test_icalevents(self):
+        from datetime import datetime
+        import textwrap
+        import zoneinfo
+
+        tz = zoneinfo.ZoneInfo("Europe/Berlin")
+        ics = textwrap.dedent(
+            """\
+        BEGIN:VCALENDAR
+        VERSION:2.0
+        BEGIN:VTIMEZONE
+        TZID:Europe/Berlin
+        BEGIN:STANDARD
+        DTSTART:19701025T030000
+        RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10
+        TZNAME:CET
+        TZOFFSETFROM:+0200
+        TZOFFSETTO:+0100
+        END:STANDARD
+        BEGIN:DAYLIGHT
+        DTSTART:19700329T020000
+        RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3
+        TZNAME:CEST
+        TZOFFSETFROM:+0100
+        TZOFFSETTO:+0200
+        END:DAYLIGHT
+        END:VTIMEZONE
+        BEGIN:VEVENT
+        DTSTART;TZID=Europe/Berlin:20211129T000000
+        DTEND;TZID=Europe/Berlin:20211129T080000
+        RRULE:FREQ=WEEKLY;UNTIL=20221230T230000Z;BYDAY=MO,TU,WE
+        END:VEVENT
+        END:VCALENDAR
+        """
+        ).encode()
+
+        events = icalevents.icalevents.events(
+            string_content=ics,
+            start=datetime(2022, 1, 11, 7, 59, 59, tzinfo=tz),
+            end=datetime(2022, 1, 11, 10, 0, 1, tzinfo=tz),
+        )
+        assert len(events) == 1
