cd41279dec4b0434fe52b81e1985beced5621eef
Christian Kohlstedde <christian@kohlsted.de>
2025-11-02 14:52:04 +0100

    updates

--- uecp_test.py
+++ uecp_test.py
@@ -2,38 +2,35 @@ import asyncio
 import itertools
 from asyncio import StreamWriter
 
-from uecp.commands import ProgrammeServiceNameSetCommand, RadioTextSetCommand
+from uecp.commands import RadioTextBufferConfiguration, RadioTextSetCommand
 from uecp.frame import UECPFrame
 
 from scheduler import config
 from scheduler.rds import open_serial_writer
 
-sequence_number = itertools.cycle(range(1, 256))
 a_b_toggle = itertools.cycle([True, False])
 
 
 async def set_rds_encoder_state(uecp_writer: StreamWriter):
-    for programme in itertools.cycle(
-        [
-            ("R-AKTIV", "radioaktiv ist Hip"),
-            ("bermuda", "bermudafunk ist auch Hip"),
-        ]
-    ):
-        print(f"Set ps to {programme}")
-        command = ProgrammeServiceNameSetCommand(ps=programme[0])
+    frame = UECPFrame()
+    frame.add_command(
+        RadioTextSetCommand(
+            text="radioaktiv - Campusradio Rhein-Neckar e.V.",
+            number_of_transmissions=10,
+            a_b_toggle=False,
+        ),
+        RadioTextSetCommand(
+            text="radioaktiv - Dein Abend!",
+            number_of_transmissions=10,
+            a_b_toggle=True,
+            buffer_configuration=RadioTextBufferConfiguration.APPEND,
+        ),
+    )
 
-        frame = UECPFrame()
-        frame.sequence_counter = next(sequence_number)
-        frame.add_command(command)
-        frame.add_command(
-            RadioTextSetCommand(text=programme[1], a_b_toggle=next(a_b_toggle))
-        )
-
-        data = frame.encode()
-        uecp_writer.write(data)
-        print(data.hex())
-        await asyncio.wait_for(uecp_writer.drain(), 10)
-        await asyncio.sleep(15)
+    data = frame.encode()
+    uecp_writer.write(data)
+    print(data.hex())
+    await asyncio.wait_for(uecp_writer.drain(), 10)
 
 
 async def main():
